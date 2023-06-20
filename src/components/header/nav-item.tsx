cc21bbbbf30dc2b1ae4970b768205c7e62526834
Daustinn Dev <57356474+daustinndev@users.noreply.github.com>
2023-06-20 23:08:08 +0700

    dark mode nav item

--- src/components/header/nav-item.tsx
+++ src/components/header/nav-item.tsx
@@ -7,7 +7,7 @@ const NavItem = ({ label, href }: NavItemType) => {
     <li>
       <a
         href={href}
-        className="dark:text-neutral-400 text-neutral-600 hover:text-neutral-950 py-1 group transition-colors flex items-center max-w-min"
+        className="dark:text-neutral-400 text-neutral-600 hover:text-neutral-950 dark:hover:text-neutral-50 py-1 group transition-colors flex items-center max-w-min"
       >
         <div className="h-[2px] w-4 dark:bg-neutral-500 bg-neutral-700 group-hover:w-6 transition-all group-hover:dark:bg-neutral-200 group-hover:bg-neutral-800" />
         <span className="ml-2"> {label}</span>
